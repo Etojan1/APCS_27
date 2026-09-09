dfb49fe72ab679c0e949d462ac4a186d51dfd4c2
Etojan1 <esar4168@gmail.com>
2026-09-09 20:47:27 +0000

    update

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -3,12 +3,21 @@
  *  Date: 
 */
 
-import java.util.Scanner;
+import java.util.Scanner; 
+
+
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+Scanner sc = new Scanner(System.in);
+	 
+		System.out.println("What is your name?");
+		String name = sc.nextLine();
+		System.out.println("How old are you?");
+		 int age = sc.nextInt();
+		 System.out.println("What month is your Birthday?");
+		 int month = sc.nextInt();
+		 int year = sc.nextInt();
+		 
 	}
 }
